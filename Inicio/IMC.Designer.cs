6d3b8b6c2ef746cf011e5d2f0e1a7922d2385f1c
Francisco Guedes <62257499+bfrancd236@users.noreply.github.com>
2020-05-11 19:47:10 -0300

    Adicionar arquivos de projeto.

---
+++ Inicio/IMC.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Inicio
+{
+    partial class Tela
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.TxtCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnLimpar.Location = new System.Drawing.Point(122, 100);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(83, 39);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(122, 12);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(174, 20);
+            this.txtAltura.TabIndex = 5;
+            this.txtAltura.Text = "Altura";
+            this.txtAltura.TextChanged += new System.EventHandler(this.txt01_TextChanged);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Lucida Sans Unicode", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(93, 163);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(244, 45);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "RESULTADO";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(122, 47);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(174, 20);
+            this.txtPeso.TabIndex = 10;
+            this.txtPeso.Text = "Peso";
+            // 
+            // TxtCalcular
+            // 
+            this.TxtCalcular.Location = new System.Drawing.Point(213, 100);
+            this.TxtCalcular.Name = "TxtCalcular";
+            this.TxtCalcular.Size = new System.Drawing.Size(83, 39);
+            this.TxtCalcular.TabIndex = 11;
+            this.TxtCalcular.Text = "Calcular";
+            this.TxtCalcular.UseVisualStyleBackColor = true;
+            this.TxtCalcular.Click += new System.EventHandler(this.TxtCalcular_Click);
+            // 
+            // Tela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(456, 247);
+            this.Controls.Add(this.TxtCalcular);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.btnLimpar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Tela";
+            this.Text = "Calculador de IMC";
+            this.Load += new System.EventHandler(this.Tela_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button TxtCalcular;
+    }
+}
+
---
+++ Inicio/IMC.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inicio
+{
+    public partial class Tela : Form
+    {
+        public Tela()
+        {
+            InitializeComponent();
+        }
+
+        private void Tela_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtPeso.Text = " ";
+            txtAltura.Text = " ";
+            lblResultado.Text = " ";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt01_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtCalcular_Click(object sender, EventArgs e)
+        {
+            Double num01, num02, Resultado1, Resultado2;
+            num01 = Convert.ToDouble(txtAltura.Text);
+            num02 = Convert.ToDouble(txtPeso.Text);
+            Resultado1 = num01 * num01;
+            Resultado2 = num02 / Resultado1;
+            if (Resultado2 < 16)
+            lblResultado.Text = "Magreza grave.";
+            if (Resultado2 > 16 && Resultado2 < 17)
+            lblResultado.Text = "Magreza moderada.";
+            if (Resultado2 > 17 && Resultado2 < 19)
+                lblResultado.Text = "Magreza leve.";
+            if (Resultado2 > 19 && Resultado2 < 25)
+                lblResultado.Text = "Saudável";
+            if (Resultado2 > 25 && Resultado2 < 30)
+                lblResultado.Text = "Sobrepeso";
+            if (Resultado2 > 30 && Resultado2 < 35)
+                lblResultado.Text = "Obesidade grau 1";
+            if (Resultado2 > 35 && Resultado2 < 40)
+                lblResultado.Text = "Obesidade grau 2";
+            if (Resultado2 > 40)
+                lblResultado.Text = "Obesidade grau 3";
+        }
+    }
+}
